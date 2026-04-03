04e10f10b71d0d7ee97ed33861633a38e2fb9a7c
Alan Miranda <alan.adm2011@gmail.com>
2026-04-03 20:59:39 -0300

    new: versao 03/04/2026

--- scripts/turbomachine/__init__.py
+++
@@ -1,5 +0,0 @@
-# scripts/turbomachine/__init__.py
-
-# Importa a classe principal do módulo turbomachine.py para o nível do pacote
-from .turbomachine import TurbomachineAnalyzer
-__all__ = ['TurbomachineAnalyzer']
